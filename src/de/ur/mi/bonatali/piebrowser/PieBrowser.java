3b17449aae52c8759c360bfaf14a363b78746b70
BoNatali <Natali.Bopp@stud.uni-regensburg.de>
2015-06-16 13:45:04 +0200

    created transparent scene

--- src/de/ur/mi/bonatali/piebrowser/PieBrowser.java
+++ src/de/ur/mi/bonatali/piebrowser/PieBrowser.java
@@ -3,5 +3,7 @@ package de.ur.mi.bonatali.piebrowser;
 import com.mrlonee.radialfx.colormenu.RadialColorMenu;
 
 public class PieBrowser extends RadialColorMenu{
+	
+	
 
 }
--- src/de/ur/mi/bonatali/piebrowser/PieFileBrowser.java
+++ src/de/ur/mi/bonatali/piebrowser/PieFileBrowser.java
@@ -1,5 +1,7 @@
 package de.ur.mi.bonatali.piebrowser;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -16,6 +18,7 @@ import javafx.animation.KeyValue;
 import javafx.animation.ParallelTransition;
 import javafx.animation.Timeline;
 import javafx.animation.Transition;
+import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.value.ChangeListener;
@@ -23,15 +26,44 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.ImageViewBuilder;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
 import com.mrlonee.radialfx.*;
 
-public class PieFileBrowser {
+public class PieFileBrowser extends Application{
+	
+	public static void main(final String[] args) {
+		launch(args);
+	}
+
+	private PieBrowser pie;
+	private Group container; //Gruppe von grafischen Elementen
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		container = new Group ();
+		final Scene scene = new Scene(container, Color.TRANSPARENT);
+		stage.initStyle(StageStyle.TRANSPARENT);
+		stage.setResizable(false);
+		stage.setScene(scene);
+		stage.centerOnScreen();
+		final Dimension screenSize = Toolkit.getDefaultToolkit()
+				.getScreenSize();
+		stage.setWidth(screenSize.getWidth());
+		stage.setHeight(screenSize.getHeight());
+		stage.toFront();
+		stage.setTitle("DAS IST MEINE BACHELORARBEIT, BITCH!");
+		stage.show();
+		
+	}
 
 }
